3b011d8467b10b3f14d5c34c3e5cdf1b5b375e52
lesley <lesleybanadzem2017@gmail.com>
2019-09-11 19:02:35 -0700

    added styles to the submit button

--- forms/src/component/Form.js
+++ forms/src/component/Form.js
@@ -19,12 +19,17 @@ function OnboardingForm ({values}){
 }
 
 const OnboardingFormWithFormik = withFormik({
+    //method
     mapPropsToValues({name, email, password}){
         return{
             name: name || '',
             email: email || '',
             password: password || '',
         }
+    },
+    //method
+    handleSubmit: (props)=>{
+        console.log(props)
     }
 })(OnboardingForm);
 
@@ -40,8 +45,32 @@ const FormikStyles = styled.div`
         position: absolute; 
         top: 25%;
         left: 25%;
+        padding: 32px;
+        background-color: #fff;
+        box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+        text-align: left;
         input{
-            margin: 5px 5px;
+            border-left: none;
+            height: 25px;
+            border-right: none;
+            padding: 5px 10px;
+            border: 0 0 3px 0;
+            border-top: none;
+            border-bottom: 3px solid lightgrey;
+        }
+        input:focus{
+            outline: none;
+            border-bottom: 3px solid darkgrey;
+        }
+        input[type='submit']{
+            border: 1px solid lightgrey;
+            border-radius: 20px;
+            font-size: 1rem;
+            height: 35px;
+        }
+        input[type='submit']:hover {
+            background: darkgrey;;
+            color: whitesmoke;
         }
     }
 
